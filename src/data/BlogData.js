2801a8854d09ba7523dfa731bce9dad18d55e888
Mohit <Mphogat007@gmail.com>
2024-12-22 20:36:17 +0530

    first commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,54 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"How to implement Pagination Component in React from scratch",
+        tags:["react","css"],
+        date:"5 Jan, 2021",
+        imgSrc:"https://miro.medium.com/v2/resize:fit:1400/format:webp/1*9cbovy5-k1l2QCdzlMy0fg.png",
+        link:"https://medium.com/@mohitphogat/learn-what-matters-your-react-learning-companion-on-github-889b4e317bc9"
+    },
+    {
+        id:2,
+        name:"Learn What Matters: Components, Props, and Bootstrap Integration",
+        tags:["react","styled-components"],
+        date:"5 Dec, 2024",
+        imgSrc:"https://miro.medium.com/v2/resize:fit:1304/format:webp/1*J5o5oQCeHlW9aZTCHmbsOg.png",
+        link:"https://medium.com/@mohitphogat/learn-what-matters-components-props-and-bootstrap-integration-66bf12352297"
+    },
+    {
+        id:3,
+        name:"Learn What Matters : React Beginners Guide: Setup, Folder Structure, JSX, and Essential Tools",
+        tags:["react","ReactJs","React-Setup","JSX"],
+        date:"5 Dec, 2024",
+        imgSrc:"https://miro.medium.com/v2/resize:fit:1400/format:webp/1*PakgBuZ7a7yjsUtNr0bDzA.png",
+        link:"https://medium.com/@mohitphogat/learn-what-matters-react-beginners-guide-setup-folder-structure-jsx-and-essential-tools-565ad4b87533"
+    },
+    {
+        id:4,
+        name:"Learn What Matters: Getting started with React",
+        tags:["React","Reactjs","Notes"],
+        date:"5 Dec, 2024",
+        imgSrc:"https://miro.medium.com/v2/resize:fit:1400/format:webp/1*AujxjUvL4sVj4zGcYzoaUQ.png",
+        link:"https://medium.com/@mohitphogat/master-react-learn-what-matters-step-by-step-b9d15e5e9f7d"
+    },
+    {
+        id:5,
+        name:"Learn What Matters: Branching, Merging, and Collaborations in GitHub",
+        tags:["Git","Github","Branching"],
+        date:"25 Nov, 2024",
+        imgSrc:"https://miro.medium.com/v2/resize:fit:1400/format:webp/1*SfM3Qz5mrr9ItSCwo7MmeA.png",
+        link:"https://medium.com/@mohitphogat/learn-what-matters-branching-merging-and-collaborations-in-github-06c0b6236dc8"
+    },
+    {
+        id:6,
+        name:"Learn What Matters: Getting Started with Git and GitHub",
+        tags:["Git","Github","Getting-started"],
+        date:"24 Nov, 2022",
+        imgSrc:"https://miro.medium.com/v2/resize:fit:1400/format:webp/1*0EV9Skbhp02hlqbOuGgUDA.jpeg",
+        link:"https://medium.com/@mohitphogat/learn-what-matters-getting-started-with-git-and-github-2d25598d0d73"
+
+    }
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,63 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Flux8 Labs",
+        description:"Flux8 Labs is a tech-driven platform designed to craft innovative solutions. Built with simple HTML, CSS, and JavaScript.",
+        tags:["html5","gsap","css","js"],
+        
+        demo:"https://flux8labs.com",
+        github:"https://phogat007.github.io/coming_soon/"
+    },
+    {
+        id:2,
+        name:"Scholar",
+        description:"A platform for BCA and MCA students providing papers, notes, syllabi, academic calendars, and top course recommendations.",
+        tags:["html","css","js","Gdrive"],
+        
+        demo:"https://scholar.flux8labs.com",
+        github:"https://phogat007.github.io/coming_soon/"
+        
+    },
+    {
+        id:3,
+        name:"Jawamotorcycles.com clone",
+        description:"A clone of Jawamotorcycles.com build using html, css, js. Built from scratch",
+        tags:["html","css","js"],
+        
+        demo:"https://phogat007.github.io/jawa/",
+        github:"https://github.com/Phogat007/jawa"
+    },
+    {
+        id:4,
+        name:"Duh the music box",
+        description:"A sleek website designed to showcase speakers, featuring detailed specifications, high-quality visuals, and user-friendly navigation for an immersive browsing experience",
+        tags:["html","css","js"],
+        
+        demo:"https://phogat007.github.io/duh/",
+        github:"https://github.com/Phogat007/duh"
+       
+    },{
+        id:5,
+        name:"Services Cards",
+        description:"A responsive animated service cards section. Built from scratch ",
+        tags:["html","css","js"],
+        
+        demo:"https://phogat007.github.io/services1/",
+        github:"https://github.com/Phogat007/services1"
+        
+    },
+    {
+        id:6,
+        name:"QR Code Generator",
+        description:"A reponsive WR code Generator made by using api and simple html and css. Built from scratch.",
+        tags:["html","api","css"],
+        
+        demo:"https://phogat007.github.io/QR_Code/",
+        github:"https://github.com/Phogat007/QR_Code"
+        
+    },
+    
+  
+] 
